393624c12a0c44a6806b35cc3988fc0413c5d62f
Eric Weiss <eweiss1995@gmail.com>
2018-05-06 19:08:31 -0400

    Switched to previous version of framework. Got a good frame for the site that I like!

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-//Initializing swiipable tabs
-M.AutoInit();
+$(document).ready(function () {
+    $('.slider').slider({
+        interval: 100000000000
+    });
+
+//    var indicatorValues = ['Opening Video', 'Games', 'In Depth Information'];
+//    var allIndicators = $('.indicators').find($('li'));
+//
+//    $('.indicator-item').hover(function(el) {
+//
+//        var which = $(this).attr('class');
+//        if (!which.includes('active')) {
+//            for (var i = 0; i < allIndicators.length; i++) {
+//                if (allIndicators[i] === this) {
+//                    $(this).addClass('tooltipped');
+//                    $('.tooltipped').tooltip({
+//                        tooltip: indicatorValues[i],
+//                        position: top,
+//                        delay: 50
+//                    });
+//                }
+//            }
+//        }
+//    })
+});

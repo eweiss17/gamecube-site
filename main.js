7fe723a36eda4c4746517ae97846169ae25230d3
Eric Weiss <eweiss1995@gmail.com>
2018-05-21 19:41:17 -0400

    new scrollbar, updated video

--- main.js
+++ main.js
@@ -3,23 +3,27 @@ $(document).ready(function () {
         interval: 100000000000
     });
 
-//    var indicatorValues = ['Opening Video', 'Games', 'In Depth Information'];
-//    var allIndicators = $('.indicators').find($('li'));
-//
-//    $('.indicator-item').hover(function(el) {
-//
-//        var which = $(this).attr('class');
-//        if (!which.includes('active')) {
-//            for (var i = 0; i < allIndicators.length; i++) {
-//                if (allIndicators[i] === this) {
-//                    $(this).addClass('tooltipped');
-//                    $('.tooltipped').tooltip({
-//                        tooltip: indicatorValues[i],
-//                        position: top,
-//                        delay: 50
-//                    });
-//                }
-//            }
-//        }
-//    })
+    $('#scroll-box').overlayScrollbars({
+         className: "os-theme-thick-dark"
+    });
+
+    //    var indicatorValues = ['Opening Video', 'Games', 'In Depth Information'];
+    //    var allIndicators = $('.indicators').find($('li'));
+    //
+    //    $('.indicator-item').hover(function(el) {
+    //
+    //        var which = $(this).attr('class');
+    //        if (!which.includes('active')) {
+    //            for (var i = 0; i < allIndicators.length; i++) {
+    //                if (allIndicators[i] === this) {
+    //                    $(this).addClass('tooltipped');
+    //                    $('.tooltipped').tooltip({
+    //                        tooltip: indicatorValues[i],
+    //                        position: top,
+    //                        delay: 50
+    //                    });
+    //                }
+    //            }
+    //        }
+    //    })
 });
